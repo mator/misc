8fde8539360a6bd7ea98d8a4c4c04cda3d98d309
Anatoly Pugachev <matorola@gmail.com>
2019-02-20 18:35:41 +0300

    PEP8

--- storcli-disk-check.py
+++ storcli-disk-check.py
@@ -16,8 +16,10 @@ if len(sys.argv) > 1 and sys.argv[1] == '--test':
     output = output.join(freadlist)
 else:
     try:
-        cmd = [ "/usr/bin/storcli", "/c0/vall", "show" ]
-        cp = subprocess.run(cmd, capture_output=True, universal_newlines=True, check=True)
+        cmd = ["/usr/bin/storcli", "/c0/vall", "show"]
+        cp = subprocess.run(cmd, capture_output=True,
+                            universal_newlines=True,
+                            check=True)
         output, rc, err = cp.stdout, cp.returncode, cp.stderr
     except:
         print("exception running " + ' '.join(cmd))
@@ -31,11 +33,11 @@ dash = 0
 for line in output.split('\n'):
     if line.startswith('---'):
         dash = dash+1
-    elif dash == 2 :
+    elif dash == 2:
         data = line.split(' ')
         #
         # if disk state is not Optimal , print and exit
-        if data[4] != 'Optl' :
+        if data[4] != 'Optl':
             print(output)
             quit()
 
