537dec3b7b32b6ef34fc11a12f11faae3b1751d3
Anatoly Pugachev <matorola@gmail.com>
2019-05-30 13:16:03 +0300

    enable LLDP for VMs on libvirt / kvm for explanation please see https://the-bitmask.com/2017/08/04/fwd-lldp-frames-on-linuxbridge/

---
+++ enable-stp-on-bridges.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo
+echo "enabling LLDP on virbr* bridges"
+echo
+brctl show | grep -v "bridge name" | awk '{ if ($1 ~ /virbr/) print $1}' | grep -v -- "-nic" | while read a; do
+        echo settings 0x4000 for bridge $a
+        echo 16384 > /sys/class/net/${a}/bridge/group_fwd_mask
+done
